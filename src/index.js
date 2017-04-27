1a129e06c3ba6e21ffcc92ee15aade15e003ba4f
Stanley Jones <stanley@chartboost.com>
2017-04-27 10:08:26 -0700

    Move express into main path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import { makeExecutableSchema } from 'graphql-tools';
+import 'babel-polyfill';
+import express from 'express';
+import graphql from 'express-graphql';
+import { text } from 'body-parser';
+import ssbClient from 'ssb-party';
 
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
+import schema from './schema';
 
-export default makeExecutableSchema({
-  typeDefs,
-  resolvers,
+const app = express();
+
+ssbClient({ party: { out: false } }, (err, sbot) => {
+  if (err) { throw new Error(err); }
+  app.use(text({ type: 'application/graphql' }));
+  app.use('/graphql', graphql({ schema, context: { sbot }, pretty: true }));
+  app.listen(3000);
 });
---
+++ src/schema.js
@@ -0,0 +1,9 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
+
+export default makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
