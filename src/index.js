79a22f1face25b5f501da98a12eba3d8bf206adb
Stanley Jones <stanley@chartboost.com>
2018-01-10 22:14:43 -0800

    Update packages, add GraphiQL support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill';
 import express from 'express';
-import graphql from 'express-graphql';
+import graphqlHTTP from 'express-graphql';
 import ssbClient from 'ssb-party';
 import { makeExecutableSchema } from 'graphql-tools';
 import { text } from 'body-parser';
@@ -13,7 +13,6 @@ const app = express();
 
 ssbClient({ party: { out: false } }, (err, sbot) => {
   if (err) { throw new Error(err); }
-  app.use(text({ type: 'application/graphql' }));
-  app.use('/graphql', graphql({ schema, context: { sbot }, pretty: true }));
+  app.use('/graphql', graphqlHTTP({ schema, context: { sbot }, graphiql: true }));
   app.listen(3000);
 });
